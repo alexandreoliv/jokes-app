6c4e02e9bd552f277fc708b9589af150c5f32206
alexandreoliv <alexandrebo@gmail.com>
2022-02-27 19:50:05 +0100

    add decrement jokes

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,15 +1,6 @@
 import React, { useState } from "react";
-
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import {
-	decrement,
-	increment,
-	incrementByAmount,
-	incrementAsync,
-	fetchCounterAsync,
-	incrementIfOdd,
-	selectCount,
-} from "./counterSlice";
+import { fetchCounterAsync, decrement, selectCount } from "./counterSlice";
 import styles from "./Counter.module.css";
 
 export function Counter() {
@@ -18,64 +9,55 @@ export function Counter() {
 	const [incrementAmount, setIncrementAmount] = useState("2");
 
 	const incrementValue = Number(incrementAmount) || 0;
-	const fakeJoke = {
-		id: "1",
-		type: "programming",
-		question: "How many programmers does it take to change a lightbulb?",
-		answer: "None that's a hardware problem",
-	};
+	const fakeJokes = [
+		{
+			id: "1",
+			type: "programming",
+			question:
+				"How many programmers does it take to change a lightbulb?",
+			answer: "None that's a hardware problem",
+		},
+		{
+			id: "2",
+			type: "programming",
+			question:
+				"How many programmers does it take to change a lightbulb?",
+			answer: "None that's a hardware problem",
+		},
+		{
+			id: "3",
+			type: "programming",
+			question:
+				"How many programmers does it take to change a lightbulb?",
+			answer: "None that's a hardware problem",
+		},
+	];
 
 	return (
 		<div>
 			<div className={styles.row}>
-				<button
-					className={styles.button}
-					aria-label="Decrement value"
-					onClick={() => dispatch(decrement())}
-				>
-					-
-				</button>
-				<span className={styles.value}>{count}</span>
-				<button
-					className={styles.button}
-					aria-label="Increment value"
-					onClick={() => dispatch(increment())}
-				>
-					+
-				</button>
+				{/* {count.map((joke) => (
+					<div key={joke.id}>
+						<p>ID: {joke.id}</p>
+						<p>Type: {joke.type}</p>
+						<p>Question: {joke.question}</p>
+						<p>Answer: {joke.answer}</p>
+					</div>
+				))} */}
+				{console.log(count)}
 			</div>
-			<div className={styles.row}>
-				<input
-					className={styles.textbox}
-					aria-label="Set increment amount"
-					value={incrementAmount}
-					onChange={(e) => setIncrementAmount(e.target.value)}
-				/>
-				<button
-					className={styles.button}
-					onClick={() => dispatch(incrementByAmount(incrementValue))}
-				>
-					Add Amount
-				</button>
-				<button
-					className={styles.asyncButton}
-					onClick={() => dispatch(incrementAsync(incrementValue))}
-				>
-					Add Async
-				</button>
-				<button
-					className={styles.asyncButton}
-					onClick={() => dispatch(fetchCounterAsync(fakeJoke))}
-				>
-					Add Async Joke
-				</button>
-				<button
-					className={styles.button}
-					onClick={() => dispatch(incrementIfOdd(incrementValue))}
-				>
-					Add If Odd
-				</button>
-			</div>
+			<button
+				className={styles.asyncButton}
+				onClick={() => dispatch(fetchCounterAsync(fakeJokes))}
+			>
+				Add Async Joke
+			</button>
+			<button
+				className={styles.asyncButton}
+				onClick={() => dispatch(decrement())}
+			>
+				Decrement joke
+			</button>
 		</div>
 	);
 }
